0aabb10e6febeee68cbf9bb6044bd6bdc82e278c
Avinash <avinashrcs@gmail.com>
2017-07-25 03:27:40 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class BusinessDeviceLayer
+    {
+        DeviceLayer _deviceLayer = new DeviceLayer();
+
+        void Rinse()
+        {
+            _deviceLayer.Rinse();
+        }
+
+
+    }
+}
